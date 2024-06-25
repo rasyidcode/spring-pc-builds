a873b38632cf4ccb9969b6421b619db85da7b0df
rasyidcode <rasyidcode7@gmail.com>
2024-06-25 09:24:03 +0700

    initial commit

---
+++ src/main/java/com/rasyidcode/spring_rakit_pc/SpringRakitPcApplication.java
@@ -0,0 +1,13 @@
+package com.rasyidcode.spring_rakit_pc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRakitPcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRakitPcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rasyidcode/spring_rakit_pc/SpringRakitPcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasyidcode.spring_rakit_pc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRakitPcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
